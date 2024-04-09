9858896dda54be2c073f09687893a04d2660048e
maria.akulova <maria.akulova.dragon@gmail.com>
2024-04-09 22:57:16 +0200

    feat: memoize

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -120,8 +120,16 @@ function getPolynom(...variables) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let isCached = false;
+  let saveFunc;
+  return () => {
+    if (!isCached) {
+      saveFunc = func();
+      isCached = true;
+    }
+    return saveFunc;
+  };
 }
 
 /**
