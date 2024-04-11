7d24174e3c9552be2ae48d696a26fe8ac136659c
maria.akulova <maria.akulova.dragon@gmail.com>
2024-04-11 18:55:18 +0200

    feat: partialUsingArguments

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -210,8 +210,11 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args2) => {
+    const allArgs = args1.concat(args2);
+    return fn(...allArgs);
+  };
 }
 
 /**
